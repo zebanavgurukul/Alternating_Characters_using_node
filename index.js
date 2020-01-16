ea853cff48070011673d48fb2d85842046b562d8
zebanavgurukul <zeba18@navgurukul.org>
2020-01-16 19:54:33 +0200

    updated

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+const readline = require("readline-sync");
+var path = require("path")
+app.use(express.urlencoded());
+
+app.get("/api", (req, res) => {
+    res.sendFile(path.join(__dirname + "/form.html"));
+});
+
+app.post("/api", (req, res) => {
+    var input = req.body.strings
+    function getCountDeleting(word){ 
+        var count = 0; 
+        datastore = word[0];
+        for (var i = 1, l = word.length; i < l; i++) {
+            if (word[i] === datastore) {
+                count ++ ;
+            } 
+            else { 
+                datastore = word[i]; 
+            } 
+        } 
+        return res.send(count); 
+    } 
+    console.log(getCountDeleting(input))
+});
+
+app.listen(4000, () => {
+    console.log("app listening at 4000");
+});
